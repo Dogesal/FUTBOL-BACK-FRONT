148c7bfefe3b9e1cd5895ef5e193a2635576a1db
Alan <alanjesusvalentino@gmail.com>
2024-07-02 17:49:13 -0500

    s

--- futbolistas-back-spring boot/src/main/java/com/example/futbolistas/repositories/IFutbolistaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.futbolistas.repositories;
-
-import com.example.futbolistas.models.FutbolistaModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface IFutbolistaRepository extends JpaRepository<FutbolistaModel, Long> {
-
-}
